2789f6511037b60cdd34c1465f2e7fffc8401dfb
Иван Вячеславович Нургазин <ivan.nurgazin@yandex.ru>
2026-01-09 12:42:02 +0700

    Оффтоп - обновил 4 задание, чтобы использовало ФВП

--- src/fourth_task.kt
+++ src/fourth_task.kt
@@ -1,11 +1,5 @@
 fun main(args: Array<String>) {
-    if (args.isEmpty()) {
-        println("При запуске программы необходимо указать аргументы")
-        return
-    }
-    val sortedArgs = args.sorted()
-    val groupedArgs = sortedArgs.groupingBy { it }.eachCount()
-    for (word in groupedArgs){
-        println("${word.key} ${word.value}")
+    args.sorted().groupingBy { it }.eachCount().forEach { (key, value) ->
+        println("$key $value")
     }
 }
