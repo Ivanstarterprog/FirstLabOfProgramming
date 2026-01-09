88275832b182888fe4d987fdad71036d3b92a70b
Иван Вячеславович Нургазин <ivan.nurgazin@yandex.ru>
2026-01-09 12:16:27 +0700

    #1 - первая задача теперь принимает пустые аргументы

--- src/first_task.kt
+++ src/first_task.kt
@@ -1,8 +1,4 @@
 fun main(args: Array<String>) {
-    if (args.isEmpty()) {
-        println("При запуске программы необходимо указать аргументы")
-        return
-    }
     for (word in args){
         println(word)
     }
