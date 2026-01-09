485afe7f8432d71aef34c3988fab3c1fa19b4c65
Иван Вячеславович Нургазин <ivan.nurgazin@yandex.ru>
2026-01-09 12:23:59 +0700

    #3 - обновил код второй задачи + одна цепочка вызова функций

--- src/second_task.kt
+++ src/second_task.kt
@@ -1,10 +1,3 @@
 fun main(args: Array<String>) {
-    if (args.isEmpty()) {
-        println("При запуске программы необходимо указать аргументы")
-        return
-    }
-    val sortedArgs = args.sorted()
-    for (word in sortedArgs){
-        println(word)
-    }
+    args.sorted().forEach(::println)
 }
