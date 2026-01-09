af5ef895b7ba75a0a5387406841a5ae229ca2f3e
Иван Вячеславович Нургазин <ivan.nurgazin@yandex.ru>
2026-01-09 12:26:08 +0700

    #4 - все прошлые обновления для 3 лабы + убрал лишние аргументы

--- src/third_task.kt
+++ src/third_task.kt
@@ -1,11 +1,3 @@
 fun main(args: Array<String>) {
-    if (args.isEmpty()) {
-        println("При запуске программы необходимо указать аргументы")
-        return
-    }
-    val sortedArgs = args.sorted()
-    val argsWithoutReapetedWords = sortedArgs.distinct()
-    for (word in argsWithoutReapetedWords){
-        println(word)
-    }
+    args.sorted().distinct().forEach(::println)
 }
